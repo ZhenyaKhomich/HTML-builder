037436a36406e6c49c924f96ec85d43420795fb8
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-01-22 16:49:26 +0400

    3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('node:fs/promises');
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(
+  pathFolder,
+  {
+    withFileTypes: true,
+  },
+  (el, files) => {
+    if (el) return console.error(el);
+    files.forEach((file) => {
+      path.join(__dirname, 'secret-folder', `${file.name}`);
+      if (file.isFile()) {
+        const filePath = path.join(__dirname, 'secret-folder', `${file.name}`);
+        newFileStats(filePath, file);
+      }
+    });
+  },
+);
+
+function newFileStats(filePath, file) {
+  fs.stat(filePath, (el, stats) => {
+    if (el) return console.error(el.message);
+    console.log(`${file.name} - ${path.extname(file.name)} - ${stats.size}`);
+  });
+}
